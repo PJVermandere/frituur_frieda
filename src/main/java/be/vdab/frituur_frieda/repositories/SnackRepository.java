0033bfb7c54105f9f4010b13703a205016f9c9f8
prince vermandere <prince.vermandere@outlook.be>
2021-07-27 15:09:38 +0200

    Repository

--- src/main/java/be/vdab/frituur_frieda/repositories/SnackRepository.java
+++
@@ -1,5 +0,0 @@
-package be.vdab.frituur_frieda.repositories;
-
-public interface SnackRepository {
-
-}
---
+++ src/main/java/be/vdab/frituur_frieda/repositories/SnackRepositoryInterface.java
@@ -0,0 +1,20 @@
+package be.vdab.frituur_frieda.repositories;
+
+import be.vdab.frituur_frieda.domain.Snack;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface SnackRepositoryInterface {
+    long addSnack(Snack snack);
+    long updateSnack(Snack snack);
+    long deleteSnack(long id);
+    List<Snack> getAllSnacks();
+    Optional<Snack> getOneSnack(long id);
+    long getSnackCount();
+    List<BigDecimal> getUniquePrices();
+    List<Snack> getSnackWithPrice(BigDecimal price);
+    List<Snack> getSnacksWithId(Set<Long> ids);
+}
